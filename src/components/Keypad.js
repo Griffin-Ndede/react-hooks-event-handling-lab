f4febcd0daf9c27a40e88212fc07091fb0120fb3
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-07-21 14:11:53 +0300

    completed assignement

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function handleChange(event){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <h1>Keypad</h1>
+            <input type="password" placeholder="Enter Your password" onChange={handleChange}></input>
+        </div>
     )
 }
-
 export default Keypad;
